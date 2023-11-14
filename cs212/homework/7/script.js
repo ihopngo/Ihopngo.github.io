6b4b65b22b59d748b8b10fcaa59bec961444b208
Isaac Johnson <lividmeerkat21@gmail.com>
2023-11-13 23:50:41 -0700

    final

--- cs212/homework/7/script.js
+++ cs212/homework/7/script.js
@@ -1,7 +1,33 @@
+const resultElement = document.getElementById('output');
+const textElement = document.getElementById('borzoi');
+
 function yes() {
-    document.getElementById("output") = "correct"
+    resultElement.innerText = "correct";
+    return question2()
 }
 
 function no() {
-    document.getElementById("output") = "wrong"
+    resultElement.innerText = "wrong";
+    return question2()
 }
+function question2() {
+    textElement.innerText = "Are Borzoi's fast?";
+    return;
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function generateRandomNumber() {
+    var min = parseInt(prompt("Enter the minimum value:"));
+    var max = parseInt(prompt("Enter the maximum value:"));
+
+    if (isNaN(min) || isNaN(max)) {
+      alert("Please enter valid numbers for the range.");
+      return;
+    }
+
+    var randomNumber = getRandomNumber(min, max);
+    document.getElementById('result').innerText = 'Random Number: ' + randomNumber;
+  }
