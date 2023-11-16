a0182d1708bccdc45f9129920b324dca1d71901c
Isaac Johnson <lividmeerkat21@gmail.com>
2023-11-15 22:58:07 -0700

    latest

--- cs212/homework/8/script.js
+++ cs212/homework/8/script.js
@@ -1,56 +1,83 @@
-/* const textElement = document.getElementById('text')
+const textElement = document.getElementById('text')
 const buttonsElement = document.getElementById('buttons')
 
 let collar = {}
 
 function startGame() {
-    collor = {}
-    textInfo(1)
+    collar = {}
+    showText(1)
 }
 
-function textInfo(choices) {
-    const textNode = textNodes.find(textNode => textNode.id === choices)
-    textElement.innerText = textNode.text
-    while (buttonsElement.firstChild) {
-        buttonsElement.removeChild(buttonsElement,firstChild)
+function showText(textInfo) {
+    const text = storyInfo.find((item) => item.id === textInfo).text;
+            textElement.innerHTML = `<p>${text}</p>`;
+            showOptions(textInfo);
+}
+
+function showOptions(textInfo) {
+    const options = storyInfo.find((item) => item.id === textInfo).options;
+    buttonsElement.innerHTML = '';
+    for (const option of options) {
+        const button = document.createElement('button');
+        button.innerText = option.text;
+        button.addEventListener('click', () => buttonOptions(option));
+        buttonsElement.appendChild(button);
+    }
+}
+
+
+function buttonOptions(option) {
+    if (option.setCollar) {
+        collar = Object.assign(collar, option.setCollar);
     }
 
-    textNode.options.forEach(option => {
-        if (showOption(option)) {
-            const button = document.createElement('button')
-            button.innerText = option.text
-            button.classlist.add('buttons')
-            button.addEventListener('click', () => buttonClick(buttoninfo))
-            buttonsElement.appendChild(button)
-        }
-    })
+    showText(option.nextText);
 }
 
-function showOption(option) {
-    return true
-}
-
-function buttonClick(option) {
-
-}
-
-const textNodes = [
+const storyInfo = [
     {
         id: 1,
-        text: 'Your a baby borzoi at petsmart you can go',
+        text: 'Your a baby Borzoi in a petsmart and you seen a family come in looking to adopt a dog',
         options: [
             {
                 text: 'whimper',
                 nextText: 2
             },
             {
-                text: 'sleep through it',
-                nextText: 2
+                text: 'sleep',
+                nextText: 20
             }
         ]
-    }   
-]
-  
+    },
+    {
+        id: 2,
+        text: 'You get adopted by a family for $200 and now they decide to get you a collar, do you want to keep the collar or tear it off?',
+        options: [
+            {
+                text: 'Wear the collar',
+                setCollar: { collar: true },
+                nextText: 3
+            },
+            {
+                text: 'TEAR IT OFF!!!',
+                nextText: 3
+            }
+        ]
+    },
+    {
+        id: 3,
+        text: 'Eventually after months of being with your family one of the them leave the backyard door is wide open, you start to imagine all the fun that can happen outside, a whole world to explore, just a few steps away',
+        options: [
+            {
+                text: 'Run and enjoy freedom',
+                nextText: 4
+            },
+            {
+                text: 'Stay home',
+                nextText: 15
+            }
+        ]
+    },
+];
 
-startGame() */
-
+startGame()
